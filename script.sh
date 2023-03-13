c707a2c4feec4a782bbf118e43bb48f417d862a8
kevin-roan <kevinroan@proton.me>
2023-03-13 21:02:06 +0530

    actual scripting started

--- script.sh
+++ script.sh
@@ -3,20 +3,26 @@ debloat(){
   echo "Debloating Started, Don't Unplug Your Device"
   #spinner here
 }
-
 rollback(){
   echo "Rollback Started, Don't Unplug Your Device"
  #spinner here
 }
-echo -ne "
+echo "Checking For Connected Devices"
+  adb devices 
+  echo "Press 0 To Start Debloating On The Aboue Device"
+  read confirmation 
+  if [ $confirmation == 0 ]; then
+
+ echo -ne "
  What you want to do...?
  1) Debloat Device
  2) Rollback Changes 
-"
-
+" 
 read option
 if[ $option == 1 ]; then 
   debloat
 else
  rollback 
 fi
+
+  fi
