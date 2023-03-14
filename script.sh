8e46670f204381b41714b69562bd5c2ae0c6dfe6
kevin-roan <kevinroan@proton.me>
2023-03-14 09:04:29 +0530

    add app-list

--- script.sh
+++ script.sh
@@ -2,7 +2,8 @@
 debloat(){
   echo "Debloating Started, Don't Unplug Your Device"
   sleep 2
-}
+  echo "Be Patient..."
+adb shell << appl-list.txt
 rollback(){
   echo "Rollback Started, Don't Unplug Your Device"
  #spinner here
