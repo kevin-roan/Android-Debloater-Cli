eff3b21c179ae876d7fb81561af5dd09b4f8b1e7
kevin-roan <kevinroan@proton.me>
2023-03-19 23:38:04 +0530

    add option for new app addition

--- debloat-android-cli.sh
+++ debloat-android-cli.sh
@@ -12,6 +12,11 @@ rollback(){
   $packages cat app-list.txt
   adb shell pm install-existing $(head -n 1 $packages)
 }
+insertnewapp(){
+  echo "Enter Application Package Name"
+  read appname
+  echo $appname > app-list.txt
+}
 echo "Checking For Connected Devices"
 sleep 2
   adb devices 
@@ -19,10 +24,14 @@ sleep 2
  What you want to do...?
  1) Debloat Device
  2) Rollback Changes 
+ 3) Insert New App To Debloat List
 " 
 read option
 if [ $option == 1 ]; then 
   debloat
 else
+  [ $option == 2]
  rollback 
+else 
+  insertnewapp
 fi
